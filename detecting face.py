4a0ae652ab24216bee97a3f2ea91dc0af2334ffe
Hassan <148881982+hasanshahidd@users.noreply.github.com>
2024-12-05 19:38:54 +0500

    detecting face.py

--- detecting face.py
+++ detecting face.py
@@ -1,57 +1,49 @@
 import cv2
-from keras.models import model_from_json # type: ignore
+from keras.models import model_from_json
 import numpy as np
+import os
 
-# Load the trained model
 json_file = open("emotiondetector.json", "r")
 model_json = json_file.read()
 json_file.close()
 model = model_from_json(model_json)
 model.load_weights("emotiondetector.h5")
-
-# Load the face detection model (Haar Cascade)
 haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(haar_file)
-
-# Function to preprocess the face image
+save_directory = 'captured_faces'
+if not os.path.exists(save_directory):
+    os.makedirs(save_directory)
 def extract_features(image):
     feature = np.array(image)
     feature = feature.reshape(1, 48, 48, 1)
     return feature / 255.0
-
-# Initialize webcam
 webcam = cv2.VideoCapture(0)
-
-# Emotion labels
 labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
-
+image_counter = 0
 while True:
-    ret, im = webcam.read()  # 'ret' is unused, we can skip it
+    ret, im = webcam.read()
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    
-    # Detect faces
     faces = face_cascade.detectMultiScale(im, 1.3, 5)
-    
     try:
         for (p, q, r, s) in faces:
-            # Crop the face region and preprocess it
             image = gray[q:q+s, p:p+r]
             cv2.rectangle(im, (p, q), (p+r, q+s), (255, 0, 0), 2)
             image = cv2.resize(image, (48, 48))
             img = extract_features(image)
-            
-            # Predict emotion
             pred = model.predict(img)
             prediction_label = labels[pred.argmax()]
+           cv2.putText(im, prediction_label, (p-10, q-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
+       cv2.imshow("Output", im)
 
-            # Display the predicted emotion label
-            cv2.putText(im, prediction_label, (p-10, q-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
-        
-        cv2.imshow("Output", im)
-        if cv2.waitKey(27) & 0xFF == ord('q'):  # Exit on 'q' key press
+        if len(faces) > 0:
+            face_image_filename = os.path.join(save_directory, f"face_.jpg")
+            cv2.imwrite(face_image_filename, im[q:q+s, p:p+r])
+            image_counter += 1
+
+        if cv2.waitKey(27) & 0xFF == ord('q'):
             break
     except cv2.error:
         pass
 
-webcam.release()  # Release the webcam when done
-cv2.destroyAllWindows()  # Close any open windows
+webcam.release()
+cv2.destroyAllWindows()
